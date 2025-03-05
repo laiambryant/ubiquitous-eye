54ef603dad6c833c1c1313f7c6110d989682180d
Liam Bryant <liam.bryant@dgsspa.com>
2025-03-05 09:53:33 +0100

    Added Prometheus and yaeger to the docker compose and added the two configuration ymls, need to connect them to the application

--- src/packages/server/server.go
+++ src/packages/server/server.go
@@ -33,7 +33,6 @@ func RunServer() {
 func getHttpOtelHandler() http.Handler {
 	mux := http.NewServeMux()
 	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-		// Configure the "http.route" for the HTTP instrumentation.
 		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
 		mux.Handle(pattern, handler)
 	}
