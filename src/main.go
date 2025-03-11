4c29b8e9f6d99a0671c6c9a2a9b92423e39fcb45
Liam Bryant <liam.bryant@dgsspa.com>
2025-03-11 17:32:34 +0100

    Added the statically deployable index.html and a batch mode to create the new static file

--- src/main.go
+++ src/main.go
@@ -6,22 +6,30 @@ import (
 	"os"
 	"os/signal"
 	"ubiquitous-eye/packages/server"
+	"ubiquitous-eye/packages/services"
 	"ubiquitous-eye/packages/telemetry"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	otelShutdown, err := telemetry.SetupOTelSDK(ctx)
-
-	if err != nil {
-		return
+	if os.Args != nil {
+		if len(os.Args) > 1 {
+			if os.Args[1] == "server-mode" {
+				ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+				defer stop()
+				otelShutdown, err := telemetry.SetupOTelSDK(ctx)
+				if err != nil {
+					return
+				}
+				defer func() {
+					err = errors.Join(err, otelShutdown(context.Background()))
+				}()
+				server.RunServer()
+			}
+		} else {
+			print("Creating deploy site\n")
+			services.CreateDeploySite()
+			print("Deploy site created")
+		}
 	}
-	defer func() {
-		err = errors.Join(err, otelShutdown(context.Background()))
-	}()
-
-	server.RunServer()
 
 }
--- src/packages/services/services.go
+++ src/packages/services/services.go
@@ -1,1 +1,102 @@
 package services
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"ubiquitous-eye/packages/model/response"
+	"ubiquitous-eye/packages/utils"
+)
+
+func GetData() (response.UserAPIResponse, []response.UserRepoApiResponse, error) {
+	user, err := getUserData()
+	if err != nil {
+		return response.UserAPIResponse{}, []response.UserRepoApiResponse{}, err
+	}
+	user_repo, err := getUserRepoData()
+	if err != nil {
+		return response.UserAPIResponse{}, []response.UserRepoApiResponse{}, err
+	}
+	if utils.DEBUG {
+		printUserData(user, user_repo)
+	}
+	return user, user_repo, nil
+}
+
+func getUserData() (response.UserAPIResponse, error) {
+	var user response.UserAPIResponse
+	user_resp, err := http.Get(utils.GITHUB_USER_LBRYANT)
+	if err != nil {
+		log.Print(err)
+		return response.UserAPIResponse{}, err
+	}
+	defer user_resp.Body.Close()
+	str, err := io.ReadAll(user_resp.Body)
+	if err != nil {
+		log.Print(err)
+		return response.UserAPIResponse{}, err
+	}
+	err = json.Unmarshal(str, &user)
+	if err != nil {
+		log.Print(err)
+		return response.UserAPIResponse{}, err
+	}
+	return user, nil
+}
+
+func getUserRepoData() ([]response.UserRepoApiResponse, error) {
+	var usrRep []response.UserRepoApiResponse
+	user_project_resp, err := http.Get(utils.GITHUB_USER_LBRYANT + "/repos")
+	if err != nil {
+		log.Print(err)
+		return []response.UserRepoApiResponse{}, err
+	}
+	str, err := io.ReadAll(user_project_resp.Body)
+	if err != nil {
+		log.Print(err)
+		return []response.UserRepoApiResponse{}, err
+	}
+	err = json.Unmarshal(str, &usrRep)
+	if err != nil {
+		log.Print(err)
+		return []response.UserRepoApiResponse{}, err
+	}
+	return usrRep, nil
+}
+
+func printUserData(user response.UserAPIResponse, repos []response.UserRepoApiResponse) {
+	log.Printf("User: %v\n", user)
+	for _, repo := range repos {
+		log.Printf("Repo: %+v\n", repo)
+	}
+}
+
+func CreateDeploySite() {
+	user, repos, err := GetData()
+	if err != nil {
+		log.Fatal("Failed to load data", err)
+		return
+	}
+	data := struct {
+		User  response.UserAPIResponse
+		Repos []response.UserRepoApiResponse
+	}{
+		User:  user,
+		Repos: repos,
+	}
+
+	// Create file in the deploy folder
+	file, err := os.Create("deploy/index.html")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+	tmpl := template.Must(template.ParseFiles(utils.INDEX_HTML_PATH))
+	_ = tmpl.Execute(file, data)
+	if err != nil {
+		log.Fatal("Cannot execute template", err)
+	}
+}
